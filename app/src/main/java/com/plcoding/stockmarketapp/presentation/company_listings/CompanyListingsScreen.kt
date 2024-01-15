c87fed5bfb52dcdbe875efb69962b6e086797935
Rodrigo Bellanti <rdrg.000@gmail.com>
2024-01-15 11:55:25 -0300

    di

--- app/src/main/java/com/plcoding/stockmarketapp/presentation/company_listings/CompanyListingsScreen.kt
+++ app/src/main/java/com/plcoding/stockmarketapp/presentation/company_listings/CompanyListingsScreen.kt
@@ -1,23 +1,28 @@
 package com.plcoding.stockmarketapp.presentation.company_listings
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Divider
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.google.accompanist.swiperefresh.SwipeRefresh
-import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 @Destination(start = true)
 fun CompanyListingsScreen(
@@ -25,8 +30,9 @@ fun CompanyListingsScreen(
     viewModel: CompanyListingsViewModel = hiltViewModel()
 ) {
 
-    val swipeRefreshState = rememberSwipeRefreshState(
-        isRefreshing = viewModel.state.isRefreshing
+    val pullRefreshState = rememberPullRefreshState(
+        refreshing = viewModel.state.isRefreshing,
+        onRefresh = { viewModel.onEvent(CompanyListingsEvent.Refresh) }
     )
     val state = viewModel.state
     Column(
@@ -48,11 +54,8 @@ fun CompanyListingsScreen(
             maxLines = 1,
             singleLine = true
         )
-        SwipeRefresh(
-            state = swipeRefreshState,
-            onRefresh = {
-                viewModel.onEvent(CompanyListingsEvent.Refresh)
-            }
+        Box(
+            Modifier.pullRefresh(pullRefreshState)
         ) {
             LazyColumn(
                 modifier = Modifier.fillMaxSize()
@@ -75,6 +78,11 @@ fun CompanyListingsScreen(
                     }
                 }
             }
+            PullRefreshIndicator(
+                viewModel.state.isRefreshing,
+                pullRefreshState,
+                Modifier.align(Alignment.TopCenter)
+            )
         }
     }
 }
