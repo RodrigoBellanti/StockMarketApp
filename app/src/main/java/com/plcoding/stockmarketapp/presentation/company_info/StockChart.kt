7438129f9cc46ea955681357fcdc8d8cee9a0fbd
Rodrigo Bellanti <rdrg.000@gmail.com>
2024-01-15 17:00:10 -0300

    CompanyInfoScreen

--- app/src/main/java/com/plcoding/stockmarketapp/presentation/company_info/StockChart.kt
+++ app/src/main/java/com/plcoding/stockmarketapp/presentation/company_info/StockChart.kt
@@ -65,7 +65,7 @@ fun StockChart(
             }
         }
         val priceStep = (upperValue - lowerValue) / 5f
-        (1..5).forEach { i ->
+        (1..4).forEach { i ->
             drawContext.canvas.nativeCanvas.apply {
                 drawText(
                     round(lowerValue + priceStep * i).toString(),
